c4b1e64abfd1481088c4b15898c5f6ddc3563421
miguellatorre-afk <a28715@svalero.com>
2026-02-06 13:58:30 +0100

    OpenAPI empezado

--- src/main/java/com/svalero/ecotip/dto/AnimalInDto.java
+++ src/main/java/com/svalero/ecotip/dto/AnimalInDto.java
@@ -1,6 +1,7 @@
 package com.svalero.ecotip.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,16 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AnimalInDto {
+    @NotBlank(message = "Name is mandatory")
     private String nombre;
+    @NotBlank(message = "Specie is mandatory")
     private String especie;
+    @NotBlank(message = "Weight is mandatory")
     private float peso;
-    private float altura;
+    @NotBlank(message = "In danger property is mandatory")
     private boolean EnPeligro;
+    @NotBlank(message = "Fecha is mandatory")
     private LocalDate fechaAvistamiento;
+    @NotBlank(message = "Apadrinado condition is mandatory")
     private  boolean apadrinado;
 }
